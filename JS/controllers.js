f17e5694c9d96d4170c1bedd7e293b9b4617f61e
Kevin Tunge <kevintunge12@hotmail.com>
2024-09-11 13:07:20 +0200

    Update controllers.js

--- JS/controllers.js
+++ JS/controllers.js
@@ -5,6 +5,24 @@ function drive(){
 function receive(){
     ;
 }
+
+// Hilslist //
 function greet(){
     ;
+}
+function howYouDoing(){
+    ;
+}
+function whatsUp(){
+    ;
+}
+function visitMe(){
+    ;
+}
+
+// Hvis du vinner //
+function congrats(){
+    if (coolnessBar=100){
+        message = "Gratulerer! Du vant!"
+    }
 }
