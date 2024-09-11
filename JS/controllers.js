da19191b66efaa145bc7a36411da84bdae73cae0
Kevin Tunge <kevintunge12@hotmail.com>
2024-09-11 12:56:43 +0200

    update model etc.

---
+++ JS/controllers.js
@@ -0,0 +1,10 @@
+//Controllers
+function drive(){
+    ;
+}
+function receive(){
+    ;
+}
+function greet(){
+    ;
+}
---
+++ JS/model.js
@@ -0,0 +1,4 @@
+//Model
+let message="";
+let coolnessBar="0%";
+let fuelBar="100%";
