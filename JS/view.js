3ffc1be18f751e633812033275341aa77a4ed5cf
haakonerol <haakon.erol@gmail.com>
2024-09-11 12:26:09 +0200

    update index

--- JS/view.js
+++ JS/view.js
@@ -2,23 +2,23 @@ function updateView() {
   document.getElementById("app").innerHTML = `
         <div id="coolness">
           <div id="coolnessBar">50%</div>
-          </div>
-          <div id="fuel">
+        </div>
+        <div id="fuel">
           <div id="fuelBar">100%</div>
-          </div>
-          <div id="congrats">
+        </div>
+        <div id="congrats">
             <h1> Gratulerer! du vant</h1>                
-          </div>
-          <div>                                   
-          <img id="car" src="./images/car.gif"  alt="car"/>
-          <img class="kompis" src="./images/images.jpg" alt="kompis"/>
+        </div>
+        <div>                                   
+          <img id="car" src="../images/car.gif"  alt="car"/>
+          <img class="kompis" src="../images/images.jpg" alt="kompis"/>
           
-          </div>            
+        </div>            
           <p id="message"></p>
           
           <button class="button" onclick="drive()"> KJØR Forbi</button>
           <button class="button" onclick="receive()"> Ta i Mot</button>
-          <div id="dropdown" >
+        <div id="dropdown" >
           <button class="button" onclick="greet()"> Hilse </button>
             <ul id="dyrList">
               <li>sdfghjk</li>
@@ -26,7 +26,7 @@ function updateView() {
               <li>sdfghjk</li>
               <li>sdfghjk</li>
             </ul>
-          </div>
+        </div>
           `;
 }
 
